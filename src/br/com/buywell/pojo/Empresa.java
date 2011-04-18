1c611482823bc4074f79537fe74d3c433d536fd8
Diego Patrick da Silva Santos <diegotrb2008@gmail.com>
2011-04-17 22:19:21 -0300

    Commit inicial, projeto base

---
+++ src/br/com/buywell/pojo/Empresa.java
@@ -0,0 +1,17 @@
+package br.com.buywell.pojo;
+
+import java.io.Serializable;
+
+public class Empresa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer codigo;
+	private String cnpj;
+	private String nome;
+	private String descricao;
+	private String ramo;
+	private String site;
+	private String email;
+	
+}
